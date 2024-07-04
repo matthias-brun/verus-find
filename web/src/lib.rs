2e0a20ab8bc713b38a2ec05826d608d2c282d971
Matthias <matthias.brun@inf.ethz.ch>
2024-07-04 20:37:04 +0000

    some qol

--- web/src/lib.rs
+++ web/src/lib.rs
@@ -1,10 +1,10 @@
 use leptos::*;
 use leptos_dom::log;
+use syn::spanned::Spanned;
 use syn_verus as syn;
 use verus_find_lib::matching;
 
-#[component]
-pub fn FmtTokenView(tok: matching::fmt::FmtToken) -> impl IntoView {
+pub fn token_to_view(tok: matching::fmt::FmtToken) -> impl IntoView {
     match tok {
         matching::fmt::FmtToken::Newline => Fragment::new(vec![view! { <br /> }.into_view()]),
         matching::fmt::FmtToken::Text(s, matching::fmt::HLToken(d)) => {
@@ -27,18 +27,16 @@ pub fn MatchesView(matches: ReadSignal<Vec<matching::Match>>) -> impl IntoView {
                 key=|m| m.hash()
                 children=move |m| {
                     view! {
+                        <hr />
                         <div>
                             <div style="color:green; font-weight: bold;">
                             {m.format_location_line()}
+                            <a href={format!("https://verus-lang.github.io/verus/verusdoc/src/{}.html#{}", m.file(), m.sig().span().start().line)} target="_blank">[source]</a>
                             </div>
                             <For
                                 each=move || m.as_fmt_tokens()
                                 key=|m| m.hash()
-                                children=move |tok| view! {
-                                    <FmtTokenView
-                                        tok=tok
-                                    />
-                                }
+                                children=move |tok| token_to_view(tok)
                             ></For>
                         </div><br />
                     }
@@ -54,24 +52,41 @@ pub fn VerusFindComponent(files: Vec<(String, syn::File)>) -> impl IntoView {
     let (expr, set_expr) = create_signal("".to_string());
     let (sig, set_sig) = create_signal("".to_string());
     let (matches, set_matches) = create_signal(vec![]);
+    let (err_expr, set_err_expr) = create_signal("".to_string());
+    let (err_sig, set_err_sig) = create_signal("".to_string());
 
+    let form_submit = move |ev: leptos::ev::SubmitEvent| {
+        ev.prevent_default();
+        set_matches.set(get_matches(
+            &files,
+            expr.get(),
+            sig.get(),
+            set_err_expr,
+            set_err_sig,
+        ));
+    };
     view! {
         <div>
-            <span>Search expression:</span>
-            <input type="text"
-                on:input=move |ev| {
-                    log!("Expr: {}", &event_target_value(&ev));
-                    set_expr.set(event_target_value(&ev));
-                }
-            />
-            <span>Search signature:</span>
-            <input type="text"
-                on:input=move |ev| {
-                    log!("Sig: {}", &event_target_value(&ev));
-                    set_sig.set(event_target_value(&ev));
-                }
-            />
-            <button on:click=move |_| set_matches.set(get_matches(&files, expr.get(), sig.get()))>"Search"</button>
+            <form on:submit=form_submit>
+                <span>Search expression:</span>
+                <input type="text" class={move || if err_expr.get().is_empty() { "" } else { "err" }}
+                    on:input=move |ev| {
+                        log!("Expr: {}", &event_target_value(&ev));
+                        set_expr.set(event_target_value(&ev));
+                        set_err_expr.set("".to_string());
+                    }
+                /><span class="err_msg">{move || err_expr.get()}</span><br />
+                <span>Search signature:</span>
+                <input type="text" class={move || if err_sig.get().is_empty() { "" } else { "err" }}
+                    on:input=move |ev| {
+                        log!("Sig: {}", &event_target_value(&ev));
+                        set_sig.set(event_target_value(&ev));
+                        set_err_sig.set("".to_string());
+                    }
+                /><span class="err_msg">{move || err_sig.get()}</span><br />
+                <button>"Search"</button>
+            </form>
+            <hr />
             <div>"Results: "<br />
                 <MatchesView
                 matches=matches
@@ -81,12 +96,24 @@ pub fn VerusFindComponent(files: Vec<(String, syn::File)>) -> impl IntoView {
     }
 }
 
-fn get_matches(files: &[(String, syn::File)], expr: String, sig: String) -> Vec<matching::Match> {
+fn get_matches(
+    files: &[(String, syn::File)],
+    expr: String,
+    sig: String,
+    set_err_expr: WriteSignal<String>,
+    set_err_sig: WriteSignal<String>,
+) -> Vec<matching::Match> {
     if expr.is_empty() && sig.is_empty() {
         vec![]
     } else {
         let parsed_expr: Option<syn::Expr> = syn::parse_str(&expr).ok();
         let parsed_sig: Option<syn::Signature> = syn::parse_str(&sig).ok();
+        if !expr.is_empty() && parsed_expr.is_none() {
+            set_err_expr.set("Couldn't parse expression".to_string());
+        }
+        if !sig.is_empty() && parsed_sig.is_none() {
+            set_err_sig.set("Couldn't parse signature".to_string());
+        }
         if parsed_expr.is_none() && parsed_sig.is_none() {
             log!("Expr and sig are none");
             vec![]
